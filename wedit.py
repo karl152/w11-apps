221944590e4ef88335ba31bfd726d7fba0dcb2ec
Karl <karl.david.wes@icloud.com>
2026-02-28 00:30:57 +0100

    [Wedit] add line number indicator

--- wedit.py
+++ wedit.py
@@ -12,6 +12,10 @@ def writeToConsole(string):
     Console.configure(state="normal")
     Console.insert(tk.END, string + "\n")
     Console.configure(state="disabled")
+def updateLineNumber():
+    LN = str(TextBox.index("insert").split(".")[0])
+    Linenumber.config(text="L" + LN)
+    Window.after(200, updateLineNumber)
 def close():
     global QuitQuestion
     if QuitQuestion == True:
@@ -65,20 +69,23 @@ ttk.Button(Window, text="Quit", command=close).grid(row=0, column=0, columnspan=
 ttk.Button(Window, text="Save", command=save).grid(row=0, column=2)
 ttk.Button(Window, text="Load", command=load).grid(row=0, column=3)
 PathInput = ttk.Entry(Window, textvariable=PathInputText)
-PathInput.grid(row=0, column=4, sticky="ew")
+PathInput.grid(row=0, column=4, columnspan=2, sticky="ew")
 SeparatorRows = [1, 3, 5, 7]
 for row in SeparatorRows:
-    ttk.Separator(Window, orient="horizontal").grid(row=row, column=0, columnspan=5, sticky="ew")
+    ttk.Separator(Window, orient="horizontal").grid(row=row, column=0, columnspan=6, sticky="ew")
 ttk.Label(Window, text="Use Control-S and Control-R to Search.").grid(row=2, column=0, columnspan=5)
 Console = tk.Text(Window, height=3, state="disabled")
-Console.grid(row=4, column=0, columnspan=5, sticky="ew")
+Console.grid(row=4, column=0, columnspan=6, sticky="ew")
 Status = ttk.Label(Window, text="no file yet")
 Status.grid(row=6, column=0, columnspan=5)
+Linenumber = ttk.Label(Window, text="L1")
+Linenumber.grid(row=6, column=5, sticky="e", padx=25)
 ScrollBar = ttk.Scrollbar(Window, orient="vertical")
 ScrollBar.grid(row=8, column=0, sticky="sn")
 TextBox = tk.Text(Window, yscrollcommand=ScrollBar.set)
 ScrollBar.config(command=TextBox.yview)
-TextBox.grid(row=8, column=1, columnspan=4, sticky="news")
+TextBox.grid(row=8, column=1, columnspan=5, sticky="news")
 if len(sys.argv) > 1:
     loadfile(sys.argv[1])
+Window.after(512, updateLineNumber)
 Window.mainloop()
