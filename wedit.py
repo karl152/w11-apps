fc41281a90854d4363dac7a86829edd31cf2f65c
Karl <karl.david.wes@icloud.com>
2026-03-17 23:12:21 +0100

    [Wedit] wxPython
    The W in Wedit doesn't only stand for Windows, it also stands for Wayland. I'm switching to wxPython as a GUI toolkit, that means native controls on Windows and GTK on Linux. GTK has Wayland, so here we go, this is my first ever native Wayland application. wxPython is still very new to me, but as you can see, the UI is almost like before (except for some column and spacing related stuff and the missing separators that likely won't come back). So the UI is almost done, binding buttons shouldn't be that hard, and the logic in the backend is almost the same, Python stays Python.

--- wedit.py
+++ wedit.py
@@ -1,95 +1,36 @@
 # Copyright (C) 2026 karl152
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import tkinter as tk
-from tkinter import ttk, filedialog
-import sys
+import wx
 
-QuitQuestion = True
-FilePath = ""
+class myFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(None, title="Wedit")
+        panel = wx.Panel(self)
+        QuitButton = wx.Button(panel, label="Quit")
+        SaveButton = wx.Button(panel, label="Save")
+        LoadButton = wx.Button(panel, label="Load")
+        PathEntry = wx.TextCtrl(panel)
+        InfoText = wx.StaticText(panel, label="Use Control-S and Control-R to Search.", style=wx.ALIGN_CENTER)
+        Console = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        PathText = wx.StaticText(panel, label="no file yet", style=wx.ALIGN_CENTER)
+        LineThing = wx.StaticText(panel, label="L1", style=wx.ALIGN_CENTER)
+        TextArea = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        sizer = wx.GridBagSizer(4, 6)
+        sizer.Add(QuitButton, pos=(0, 0))
+        sizer.Add(SaveButton, pos=(0, 1))
+        sizer.Add(LoadButton, pos=(0, 2))
+        sizer.Add(PathEntry, pos=(0, 3), span=(1, 3), flag=wx.EXPAND)
+        sizer.Add(InfoText, pos=(1, 0), span=(1, 6), flag=wx.EXPAND)
+        sizer.Add(Console, pos=(2, 0), span=(1, 6), flag=wx.EXPAND)
+        sizer.Add(PathText, pos=(3, 0), span=(1, 3), flag=wx.EXPAND)
+        sizer.Add(LineThing, pos=(3, 5))
+        sizer.Add(TextArea, pos=(4, 0), span=(1, 6), flag=wx.EXPAND)
+        sizer.AddGrowableCol(4)
+        sizer.AddGrowableRow(4)
+        panel.SetSizer(sizer)
 
-def writeToConsole(string):
-    Console.configure(state="normal")
-    Console.insert(tk.END, string + "\n")
-    Console.configure(state="disabled")
-def updateLineNumber():
-    LN = str(TextBox.index("insert").split(".")[0])
-    Linenumber.config(text="L" + LN)
-    Window.after(200, updateLineNumber)
-def close():
-    global QuitQuestion
-    if QuitQuestion == True:
-        QuitQuestion = False
-        writeToConsole("Click again to quit.")
-    else:
-        Window.destroy()
-def save():
-    if PathInput.get() == "":
-        file = filedialog.asksaveasfilename(parent=Window)
-    else:
-        file = PathInput.get()
-    try:
-        with open(file, "w", encoding="utf-8") as f:
-            f.write(TextBox.get(1.0, tk.END).rstrip())
-        Status.config(text=file)
-        PathInputText.set(value=file)
-    except:
-        writeToConsole("Couln't save " + file)
-    TextBox.focus_set()
-def load():
-    if PathInput.get() == "":
-        file = filedialog.askopenfilename(parent=Window)
-    else:
-        file = PathInput.get()
-    try:
-        with open(file, "r", encoding="utf-8") as FileContent:
-            TextBox.delete(1.0, tk.END)
-            for line in FileContent:
-                TextBox.insert(tk.END, line)
-        Status.config(text=file)
-        PathInputText.set(value=file)
-    except:
-        writeToConsole("Couln't open " + file)
-    TextBox.focus_set()
-def loadfile(file):
-    try:
-        with open(file, "r", encoding="utf-8") as FileContent:
-            TextBox.delete(1.0, tk.END)
-            for line in FileContent:
-                TextBox.insert(tk.END, line)
-        Status.config(text=file)
-        PathInputText.set(value=file)
-    except:
-        writeToConsole("Couln't open " + file)
-    TextBox.focus_set()
-
-Window = tk.Tk()
-PathInputText = tk.StringVar()
-Window.title("Wedit")
-Window.rowconfigure(8, weight=1)
-Window.columnconfigure(4, weight=1)
-ttk.Button(Window, text="Quit", command=close).grid(row=0, column=0, columnspan=2)
-ttk.Button(Window, text="Save", command=save).grid(row=0, column=2)
-ttk.Button(Window, text="Load", command=load).grid(row=0, column=3)
-PathInput = ttk.Entry(Window, textvariable=PathInputText)
-PathInput.grid(row=0, column=4, columnspan=2, sticky="ew")
-SeparatorRows = [1, 3, 5, 7]
-for row in SeparatorRows:
-    ttk.Separator(Window, orient="horizontal").grid(row=row, column=0, columnspan=6, sticky="ew")
-ttk.Label(Window, text="Use Control-S and Control-R to Search.").grid(row=2, column=0, columnspan=5)
-Console = tk.Text(Window, height=3, state="disabled")
-Console.grid(row=4, column=0, columnspan=6, sticky="ew")
-Status = ttk.Label(Window, text="no file yet")
-Status.grid(row=6, column=0, columnspan=5)
-Linenumber = ttk.Label(Window, text="L1")
-Linenumber.grid(row=6, column=5, sticky="e", padx=25)
-ScrollBar = ttk.Scrollbar(Window, orient="vertical")
-ScrollBar.grid(row=8, column=0, sticky="sn")
-TextBox = tk.Text(Window, yscrollcommand=ScrollBar.set)
-ScrollBar.config(command=TextBox.yview)
-TextBox.grid(row=8, column=1, columnspan=5, sticky="news")
-TextBox.focus_set()
-if len(sys.argv) > 1:
-    loadfile(sys.argv[1])
-Window.after(512, updateLineNumber)
-Window.mainloop()
+app = wx.App()
+frame = myFrame()
+frame.Show()
+app.MainLoop()
