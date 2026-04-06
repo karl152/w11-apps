49d31c904a539971b66f73da145b74b31220a9d6
Karl <karl.david.wes@icloud.com>
2026-04-06 22:22:59 +0200

    [Wedit] add search & replace

--- wedit.py
+++ wedit.py
@@ -16,7 +16,7 @@ class myFrame(wx.Frame):
         SaveButton.Bind(wx.EVT_BUTTON, lambda event: self.save(self.TextArea.GetValue()) if self.PathEntry.GetValue() == "" else self.savefile(self.PathEntry.GetValue(), self.TextArea.GetValue()))
         LoadButton.Bind(wx.EVT_BUTTON, lambda event: self.load() if self.PathEntry.GetValue() == "" else self.loadfile(self.PathEntry.GetValue()))
         self.PathEntry = wx.TextCtrl(panel)
-        InfoText = wx.StaticText(panel, label="Use Control-S and Control-R to Search.")
+        InfoText = wx.StaticText(panel, label="Use Control-F and Control-R to Search.")
         self.Splitter = wx.SplitterWindow(panel)
         self.TopPanel = wx.Panel(self.Splitter)
         self.BottomPanel = wx.Panel(self.Splitter)
@@ -32,6 +32,7 @@ class myFrame(wx.Frame):
         self.TextArea.SetFont(font)
         self.TextArea.Bind(wx.EVT_KEY_UP, self.updateLineNumber)
         self.TextArea.Bind(wx.EVT_LEFT_UP, self.updateLineNumber)
+        self.TextArea.Bind(wx.EVT_KEY_DOWN, self.keyBind)
         sizer = wx.GridBagSizer(3, 4)
         sizer.Add(QuitButton, pos=(0, 0))
         sizer.Add(SaveButton, pos=(0, 1))
@@ -112,6 +113,87 @@ class myFrame(wx.Frame):
                         return False
     def SetDontCheckIfSaved(self, value):
         self.DontCheckIfSaved = value
+    def keyBind(self, event):
+        if event.ControlDown():
+            if event.KeyCode == 70 or event.KeyCode == 82:
+                SearchAndReplaceFrame(self)
+        event.Skip()
+class SearchAndReplaceFrame(wx.Dialog):
+    def __init__(self, MainFrame):
+        super().__init__(MainFrame, title="search")
+        self.panel = wx.Panel(self)
+        self.Description = wx.StaticText(self.panel, label="Use <Tab> to change fields.\nUse ^q<Tab> for <Tab>.")
+        self.BackwardButton = wx.RadioButton(self.panel, label="Backward", style=wx.RB_GROUP)
+        self.ForwardButton = wx.RadioButton(self.panel, label="Forward")
+        self.CaseSensitiveCheckbox = wx.CheckBox(self.panel, label="Case Sensitive")
+        self.CaseSensitiveCheckbox.SetValue(True)
+        self.SearchForLabel = wx.StaticText(self.panel, label="Search for:")
+        self.ReplaceWithLabel = wx.StaticText(self.panel, label="Replace with:")
+        self.SearchEntry = wx.TextCtrl(self.panel)
+        self.ReplaceEntry = wx.TextCtrl(self.panel)
+        self.SearchButton = wx.Button(self.panel, label="Search")
+        self.SearchButton.Bind(wx.EVT_BUTTON, lambda event: self.Search(MainFrame))
+        self.ReplaceButton = wx.Button(self.panel, label="Replace")
+        self.ReplaceButton.Bind(wx.EVT_BUTTON, lambda event: self.Replace(MainFrame))
+        self.ReplaceAllButton = wx.Button(self.panel, label="Replace All")
+        self.ReplaceAllButton.Bind(wx.EVT_BUTTON, lambda event: self.ReplaceAll(MainFrame))
+        self.CancelButton = wx.Button(self.panel, label="Cancel")
+        self.CancelButton.Bind(wx.EVT_BUTTON, lambda event: self.Close())
+        self.sizer = wx.GridBagSizer(5, 4)
+        self.sizer.Add(self.Description, pos=(0, 0), span=(1, 4), flag=wx.ALL, border=10)
+        self.sizer.Add(self.BackwardButton, pos=(1, 0), flag=wx.LEFT, border=5)
+        self.sizer.Add(self.ForwardButton, pos=(1, 1))
+        self.sizer.Add(self.CaseSensitiveCheckbox, pos=(1, 2), span=(1, 2), flag=wx.ALIGN_RIGHT | wx.RIGHT, border=5)
+        self.sizer.Add(self.SearchForLabel, pos=(2, 0), flag=wx.LEFT, border=5)
+        self.sizer.Add(self.ReplaceWithLabel, pos=(3, 0), flag=wx.LEFT, border=5)
+        self.sizer.Add(self.SearchEntry, pos=(2, 1), span=(1, 3), flag=wx.EXPAND | wx.RIGHT, border=10)
+        self.sizer.Add(self.ReplaceEntry, pos=(3, 1), span=(1, 3), flag=wx.EXPAND | wx.RIGHT, border=10)
+        self.sizer.Add(self.SearchButton, pos=(4, 0), flag=wx.ALIGN_RIGHT)
+        self.sizer.Add(self.ReplaceButton, pos=(4, 1), flag=wx.BOTTOM, border=10)
+        self.sizer.Add(self.ReplaceAllButton, pos=(4, 2), flag=wx.BOTTOM, border=10)
+        self.sizer.Add(self.CancelButton, pos=(4, 3), flag=wx.RIGHT, border=25)
+        self.panel.SetSizer(self.sizer)
+        self.SearchEntry.SetFocus()
+        self.panel.Fit()
+        self.Fit()
+        self.Show()
+    def ReplaceAll(self, MainFrame):
+        text = MainFrame.TextArea.GetValue()
+        text = text.replace(self.SearchEntry.GetValue(), self.ReplaceEntry.GetValue())
+        MainFrame.TextArea.SetValue(text)
+    def Replace(self, MainFrame):
+        searchtext = self.SearchEntry.GetValue()
+        replacetext = self.ReplaceEntry.GetValue()
+        if self.ForwardButton.GetValue() == True:
+            position = MainFrame.TextArea.GetValue().find(searchtext)
+        else:
+            position = MainFrame.TextArea.GetValue().rfind(searchtext)
+        if position != -1:
+            MainFrame.TextArea.SetSelection(position, position+len(searchtext))
+            MainFrame.TextArea.Replace(position, position+len(searchtext), replacetext)
+        if self.ForwardButton.GetValue() == True:
+            position = MainFrame.TextArea.GetValue().find(searchtext)
+        else:
+            position = MainFrame.TextArea.GetValue().rfind(searchtext)
+        MainFrame.TextArea.SetFocus()
+    def Search(self, MainFrame):
+        searchtext = self.SearchEntry.GetValue()
+        if self.ForwardButton.GetValue() == True:
+            position = MainFrame.TextArea.GetValue().find(searchtext, MainFrame.TextArea.GetInsertionPoint())
+        else:
+            position = MainFrame.TextArea.GetValue().rfind(searchtext, 0, MainFrame.TextArea.GetInsertionPoint())
+        if position != -1:
+            MainFrame.TextArea.SetInsertionPoint(position + len(searchtext))
+            if self.ForwardButton.GetValue() == True:
+                MainFrame.TextArea.SetSelection(position+len(searchtext), position)
+            else:
+                MainFrame.TextArea.SetSelection(position, position+len(searchtext))
+        else:
+            if self.ForwardButton.GetValue() == True:
+                MainFrame.TextArea.SetInsertionPoint(0)
+            else:
+                MainFrame.TextArea.SetInsertionPoint(len(MainFrame.TextArea.GetValue()))
+        MainFrame.TextArea.SetFocus()
 
 app = wx.App()
 frame = myFrame()
