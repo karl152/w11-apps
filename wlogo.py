27dfb79267e066ea66274ff50179801b28d82148
Karl <karl.david.wes@icloud.com>
2026-04-08 22:51:50 +0200

    add Wlogo with Windows support

---
+++ wlogo.py
@@ -0,0 +1,30 @@
+import wx, platform
+
+class WlogoFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(None, title="xlogo")
+        self.panel = wx.Panel(self)
+        self.sizer = wx.GridBagSizer(2, 2)
+        for i in range(2):
+            btn = wx.Button(self.panel, label="")
+            btn.SetBackgroundColour("blue")
+            btn.SetForegroundColour("blue")
+            self.sizer.Add(btn, pos=(0, i), flag=wx.EXPAND | wx.ALL, border=2)
+        for i in range(2):
+            btn = wx.Button(self.panel, label="")
+            btn.SetBackgroundColour("blue")
+            btn.SetForegroundColour("blue")
+            self.sizer.Add(btn, pos=(1, i), flag=wx.EXPAND | wx.ALL, border=2)
+        self.sizer.AddGrowableRow(0)
+        self.sizer.AddGrowableRow(1)
+        self.sizer.AddGrowableCol(0)
+        self.sizer.AddGrowableCol(1)
+        self.panel.SetSizer(self.sizer)
+        self.Show()
+
+if platform.system() != "Windows":
+    print("Your platform is currently not supported by Wlogo")
+app = wx.App()
+frame = WlogoFrame()
+frame.Show()
+app.MainLoop()
